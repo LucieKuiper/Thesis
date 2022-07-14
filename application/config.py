2634faf4b81e424a083eb9f9870897c531b3dbdb
Lucie <lucie-1997@hotmail.com>
2022-07-14 15:45:37 +0200

    updated to postgress database

--- application/config.py
+++ application/config.py
@@ -1,8 +1,11 @@
+import os
+import psycopg2
+
 class Config:
     SECRET_KEY = '.'
     # Configure database
     # Old SQLlite DB
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///survey_data.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lucie:citroentje@localhost:5432/pilot'
     # New Mysql
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/users'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
