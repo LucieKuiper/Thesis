312534fd2af6375b5400973ef73631976b1320ab
Lucie <lucie-1997@hotmail.com>
2022-07-20 15:32:33 +0200

    added heroku database?

--- application/config.py
+++ application/config.py
@@ -4,9 +4,7 @@ import psycopg2
 class Config:
     SECRET_KEY = '.'
     # Configure database
-    # Old SQLlite DB
 
-
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://'
+    SQLALCHEMY_DATABASE_URI = 'heroku pg:psql postgresql-globular-41149 --app lucie-thesis-pilot'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
